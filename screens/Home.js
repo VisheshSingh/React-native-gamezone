e2ab016425f5c2324c5cc69abf0c1e0d8e1543f8
Vishesh <visheshsingh16@gmail.com>
2019-12-22 17:59:31 -0600

    Passing data between screens

--- screens/Home.js
+++ screens/Home.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, FlatList } from 'react-native';
 import { globalStyles } from '../styles/globalStyles';
 
 export default function Home({ navigation }) {
+    const [reviews, setReviews] =useState([
+        {title: 'Zelda, Breath of Fresh Air', rating: 5, body: 'lorem ipsum', key: '1' },
+        {title: 'Gotta Catch Them All (again)', rating: 4, body: 'lorem ipsum', key: '2' },
+        {title: 'Not so "Final" Fantasy', rating: 3.5, body: 'lorem ipsum', key: '3' },
+    ])
 
-    const pressHandler = () => {
-        navigation.navigate('ReviewDetails')
-    }
-    
     return (
         <View style={globalStyles.container}>
-            <Text style={globalStyles.titleText}>Home Screen</Text>
-            <Button title="go to review details" onPress={pressHandler} />
+            <FlatList 
+                data={reviews} 
+                renderItem={({item}) => (
+                    <TouchableOpacity onPress={() => navigation.navigate('ReviewDetails', item)}>
+                        <Text style={globalStyles.titleText}>{item.title}</Text>
+                    </TouchableOpacity>
+                )}
+            />
         </View>
     )
 }
--- screens/ReviewDetails.js
+++ screens/ReviewDetails.js
@@ -1,22 +1,14 @@
 import React from 'react';
 import {StyleSheet, Text, View, Button} from 'react-native';
+import { globalStyles } from '../styles/globalStyles';
 
-export default function ReviewDetails({navigation}) {
-
-    const pressHandler = () => {
-        navigation.goBack()
-    }
+export default function ReviewDetails({ navigation }) {
 
     return (
-        <View style={styles.container}>
-            <Text>ReviewDetails Screen</Text>
-            <Button title="go back to home" onPress={pressHandler} />
+        <View style={globalStyles.container}>
+            <Text>{navigation.getParam('title')}</Text>
+            <Text>{navigation.getParam('body')}</Text>
+            <Text>{navigation.getParam('rating')}</Text>
         </View>
     )
 }
-
-const styles = StyleSheet.create({
-    container: {
-        padding: 24
-    }
-})
