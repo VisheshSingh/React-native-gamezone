99b03fb624d1078ff5c504ba35344d455e8b0317
Vishesh <visheshsingh16@gmail.com>
2019-12-23 14:12:56 -0600

    Drawer Navigation

--- screens/About.js
+++ screens/About.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import { globalStyles } from '../styles/globalStyles';
 
 export default function About() {
     return (
-        <View style={styles.container}>
-            <Text>About Screen</Text>
+        <View style={globalStyles.container}>
+            <Text style={globalStyles.titleText}>About Screen</Text>
+            <Text style={globalStyles.paragraphText}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Text>
         </View>
     )
 }
-
-const styles = StyleSheet.create({
-    container: {
-        padding: 24
-    }
-})
